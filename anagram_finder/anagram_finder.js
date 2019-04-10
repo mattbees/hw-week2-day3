9da660e8c8b2fa49bcd5d4f12257fd807d043580
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-10 23:59:33 +0100

    Anagram extension: 2 passing

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,10 +1,26 @@
 class AnagramFinder{
 
-  constructor (word) {}
+  constructor (word) {
+    this.word = word; // toLowerCase()?
+  };
 
   findAnagrams(otherWords) {
+    return otherWords.filter(otherWord => {
+      const otherSplit = otherWord.split('');
+      return otherSplit.every(letter => {
+        return this.word.includes(letter);
+      });
+    });
+  };
 
-  }
-}
+};
 
 module.exports = AnagramFinder;
+
+
+
+// isPangram() {
+//   return this.alphabet.every(letter => {
+//     return this.phrase.toLowerCase().includes(letter);
+//   });
+// };
