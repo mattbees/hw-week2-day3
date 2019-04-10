81cc383925216adaf839b547a3c1b150cd8e8f2b
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-11 00:06:11 +0100

    Extensions: 5 passing

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,12 +1,12 @@
 class AnagramFinder{
 
   constructor (word) {
-    this.word = word;
+    this.word = word.toLowerCase();
   };
 
   findAnagrams(otherWords) {
     return otherWords.filter(otherWord => {
-      const otherSplit = otherWord.split('');
+      const otherSplit = otherWord.toLowerCase().split('');
       return otherSplit.every(letter => {
         return this.word.includes(letter)
           && (otherWord.length === this.word.length);
