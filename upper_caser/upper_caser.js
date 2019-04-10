87a199717a9e7385b5392d9f83dc2522a2542168
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-10 22:45:17 +0100

    add solution for upper caser

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,11 +1,14 @@
 class UpperCaser{
 
-  constructor (words) {}
+  constructor (words) {
+    this.words = words;
+  };
 
   toUpperCase() {
-
-  }
-
-}
+    return this.words.map(word => {
+      return word.toString().toUpperCase();
+    });
+  };
+};
 
 module.exports = UpperCaser;
