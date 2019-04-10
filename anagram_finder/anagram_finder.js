2e6c918730235b1a4f48cbfbef061b0e285c3675
Matthew <a9852147@9852147s-MacBook-Pro.local>
2019-04-11 00:03:43 +0100

    Extensions: 4 passing

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,14 +1,15 @@
 class AnagramFinder{
 
   constructor (word) {
-    this.word = word; // toLowerCase()?
+    this.word = word;
   };
 
   findAnagrams(otherWords) {
     return otherWords.filter(otherWord => {
       const otherSplit = otherWord.split('');
       return otherSplit.every(letter => {
-        return this.word.includes(letter);
+        return this.word.includes(letter)
+          && (otherWord.length === this.word.length);
       });
     });
   };
@@ -16,11 +17,3 @@ class AnagramFinder{
 };
 
 module.exports = AnagramFinder;
-
-
-
-// isPangram() {
-//   return this.alphabet.every(letter => {
-//     return this.phrase.toLowerCase().includes(letter);
-//   });
-// };
